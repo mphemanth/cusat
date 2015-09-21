e3938f4962d78824a4bcb1d57ac736761b10c882
hemanth <mphemanth@gmail.com>
2015-09-22 00:15:22 +0530

    implemented code to extract words from files and create a histogram

--- irws/huffman.py
+++ irws/huffman.py
@@ -1,1 +1,15 @@
 # application to do huffman coding and decoding.
+import sys
+from collections import Counter
+
+def extract(fname):
+  global pString
+  for words in open(fname,'r'):
+    pString=pString+words.split() #create a list of words from a given file
+
+if __name__=='__main__':
+	global pString # process string holder
+	pString=[] # initialize process string
+	for fIndex  in range(len(sys.argv)): # loop through files and process
+		if fIndex>0:extract(sys.argv[fIndex])
+	print Counter(pString)
