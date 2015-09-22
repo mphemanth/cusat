48b5098aac9efdea2c224e0f18b980884ae20934
mphemanth@gmail.com <jamshid99@gmail.com>
2015-09-22 11:11:28 +0530

    prepared sorted list. graph preparation in progress

--- irws/huffman.py
+++ irws/huffman.py
@@ -7,9 +7,17 @@ def extract(fname):
   for words in open(fname,'r'):
     pString=pString+words.split() #create a list of words from a given file
 
+def mkNestedLoop(list):
+	global nlist
+	print list[:2]
+	
+	
 if __name__=='__main__':
 	global pString # process string holder
+	global nlist # nested list initialization
+	nlist=[] 
 	pString=[] # initialize process string
 	for fIndex  in range(len(sys.argv)): # loop through files and process
 		if fIndex>0:extract(sys.argv[fIndex])
-	print Counter(pString)
+	sortedList=sorted(Counter(pString).most_common(),reverse=True)
+	mkNestedLoop(sortedList)
