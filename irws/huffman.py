c08f788857da4883b5ff581501de8956fc6cdadb
mphemanth <jamshid99@gmail.com>
2015-09-22 12:36:17 +0530

    nested list preperation in progress. to be checked

--- irws/huffman.py
+++ irws/huffman.py
@@ -7,17 +7,32 @@ def extract(fname):
   for words in open(fname,'r'):
     pString=pString+words.split() #create a list of words from a given file
 
-def mkNestedLoop(list):
-	global nlist
-	print list[:2]
+def readFiles(flist):
+	for file  in flist: # loop through files and process
+		extract(file)	
+		
+def mkNL(list):
+	x01=list[:2]
+	list=[(x01,x01[0][1]+x01[1][1])]+list[2:]
+	return list
+
+def mkNested(list):
+	for i in range(len(list)-1):
+		list=sorted(mkNL(list),key=getKEY)
+	return list
+	
+
+# for assisting in sort operations
+def getKEY(item):
+	return item[1]
+	
 	
 	
 if __name__=='__main__':
 	global pString # process string holder
-	global nlist # nested list initialization
-	nlist=[] 
 	pString=[] # initialize process string
-	for fIndex  in range(len(sys.argv)): # loop through files and process
-		if fIndex>0:extract(sys.argv[fIndex])
+	readFiles(sys.argv[1:])
 	sortedList=sorted(Counter(pString).most_common(),reverse=True)
-	mkNestedLoop(sortedList)
+	sortedList=mkNested(sortedList)
+	print sortedList
+	
