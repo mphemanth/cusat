7a872875ad385ff0af95b5febadf9bb83423c067
hemanth <mphemanth@gmail.com>
2015-09-30 07:20:55 +0530

    generated variable length code words

--- irws/huffman.py
+++ irws/huffman.py
@@ -14,16 +14,32 @@ def readFiles(flist):
     pString=pString+open(file01,'r').read().split()
   return sorted(Counter(pString).most_common())
 
+def append(l,b):
+	global code
+	if type(l[0])==str:code[l[0]].append(b)
+	else:	append(l[0],b)
+	if type(l[1])==str:code[l[1]].append(b)
+	else:	append(l[1],b)
+
 def c2process(l):
+	global code
+	code={}
 	while len(l)>1:
 		l=sorted(l,key=key01)
 		x=([l[0][0]]+[l[1][0]],l[0][1]+l[1][1])
+		if type(l[0][0])==str:	code[l[0][0]]=[0]
+		else:	append(l[0][0],0)
+		if type(l[1][0])==str:	code[l[1][0]]=[1]
+		else:	append(l[1][0],1)
 		del l[0:2]
 		l.append(x)
 	return l[0][0]
 
-def c3process(l01):	
-	pass
+
+
+
+
+
 	
 	
 	
@@ -32,11 +48,13 @@ def c3process(l01):
 
 
 if __name__=='__main__':
+	global code
 	sl01=readFiles(sys.argv[1:])
 	sl01=sorted(sl01,key=key01)
+	print sl01
 	sl02=c2process(sl01)
 	print sl02
-
+	print code
 	
 
 
